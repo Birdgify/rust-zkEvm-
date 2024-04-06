9485623b3d1cb2ffe2726bf5b018e29ef6508271
andropixels <navgire2001@gmail.com>
2024-04-06 22:58:25 +0530

    understanding zk token

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() {
         Candidate::new("Candidate D", "Party Z"),
         Candidate::new("Candidate D", "Party Z"),
         Candidate::new("Candidate D", "Party Z"),
+        Candidate::new("Candidate D", "Party Z"),
     ];
 
     println!("Welcome to the Electronic Voting Machine!");
